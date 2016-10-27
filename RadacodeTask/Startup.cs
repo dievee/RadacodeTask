fecd44eeddcb45e675b4077349dbf1dbc255f32a
dievee <nex7440@gmail.com>
2016-10-28 00:13:31 +0300

    added auth using facebook

---
+++ RadacodeTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RadacodeTask.Startup))]
+namespace RadacodeTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
